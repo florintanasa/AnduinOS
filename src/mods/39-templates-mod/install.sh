cfb7becdb645a173288d26b6394bde8f96b0fe13
Florin Tanasa <florin.tanasa@svgenebank.ro>
2025-06-26 14:21:46 +0300

    Adds i18n support for template directory
    Configures the template directory name based on the selected
    language.  This ensures that the template directory name is
    localized, improving the user experience for non-English users.

--- src/mods/39-templates-mod/install.sh
+++ src/mods/39-templates-mod/install.sh
@@ -2,11 +2,21 @@ set -e                  # exit on error
 set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
 
+print_ok "Declare Templates dir name as variable for specific languages..."
+case $LANG_MODE in
+    "ro_RO")
+        export TEMPLATE_DIR="Șabloane"
+        ;;
+    *)
+        export TEMPLATE_DIR="Templates"
+        ;;
+esac
+
 print_ok "Configuring templates..."
-mkdir -p /etc/skel/Templates
-touch /etc/skel/Templates/Text.txt
-touch /etc/skel/Templates/Markdown.md
-cat << 'EOF' > /etc/skel/Templates/Markdown.md
+mkdir -p /etc/skel/$TEMPLATE_DIR
+touch /etc/skel/$TEMPLATE_DIR/Text.txt
+touch /etc/skel/$TEMPLATE_DIR/Markdown.md
+cat << 'EOF' > /etc/skel/$TEMPLATE_DIR/Markdown.md
 # Title
 
 - [ ] Task 1
