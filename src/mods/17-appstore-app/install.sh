7044666fbb5348819b99acdf796a54de697401fc
Florin Tanasa <florin.tanasa@gmail.com>
2025-06-30 10:34:21 +0300

    Renames app to BRGV-OS Software
    Updates the application name from "AnduinOS Software" to "BRGV-OS Software" in the desktop entry file.
    This includes changes to the name, comment, and translations for the app store application.

--- src/mods/17-appstore-app/install.sh
+++ src/mods/17-appstore-app/install.sh
@@ -66,8 +66,8 @@ elif [ "$STORE_PROVIDER" == "snap" ]; then
     install_opt gnome-software-plugin-deb
     judge "Install snap store"
 elif [ "$STORE_PROVIDER" == "web" ]; then
-    print_ok "Adding new app called AnduinOS Software..."
-    cat << EOF > /usr/share/applications/anduinos-software.desktop
+    print_ok "Adding new app called BRGV-OS Software..."
+    cat << EOF > /usr/share/applications/brgvos-software.desktop
 [Desktop Entry]
 Name=Apps Store
 GenericName=Apps Store
@@ -91,29 +91,29 @@ Name[sv_SE]=App Store
 Name[pl_PL]=Sklep z aplikacjami
 Name[tr_TR]=Uygulama Mağazası
 Name[ro_RO]=Magazinul cu aplicații
-Comment=Browse AnduinOS's software collection and install our verified applications
-Comment[zh_CN]=浏览 AnduinOS 的软件商店并安装我们验证过的应用
-Comment[zh_TW]=瀏覽 AnduinOS 的軟體商店並安裝我們驗證過的應用
-Comment[zh_HK]=瀏覽 AnduinOS 的軟體商店並安裝我們驗證過的應用
-Comment[ja_JP]=AnduinOS のソフトウェアコレクションを閲覧し、検証済みのアプリケーションをインストールします
-Comment[ko_KR]=AnduinOS의 소프트웨어 컬렉션을 탐색하고 검증된 애플리케이션을 설치합니다
-Comment[vi_VN]=Duyệt bộ sưu tập phần mềm của AnduinOS và cài đặt các ứng dụng đã được xác minh của chúng tôi
-Comment[th_TH]=เรียกดูคอลเลกชันซอฟต์แวร์ของ AnduinOS และติดตั้งแอปพลิเคชันที่ได้รับการตรวจสอบของเรา
-Comment[de_DE]=Durchsuchen Sie die Softwarekollektion von AnduinOS und installieren Sie unsere verifizierten Anwendungen
-Comment[fr_FR]=Parcourez la collection de logiciels d'AnduinOS et installez nos applications vérifiées
-Comment[es_ES]=Explore la colección de software de AnduinOS e instale nuestras aplicaciones verificadas
-Comment[ru_RU]=Просматривайте коллекцию программного обеспечения AnduinOS и устанавливайте наши проверенные приложения
-Comment[it_IT]=Esplora la collezione di software di AnduinOS e installa le nostre applicazioni verificate
-Comment[pt_PT]=Explore a coleção de software da AnduinOS e instale nossos aplicativos verificados
-Comment[pt_BR]=Explore a coleção de software da AnduinOS e instale nossos aplicativos verificados
-Comment[ar_SA]=تصفح مجموعة البرامج الخاصة بـ AnduinOS وقم بتثبيت تطبيقاتنا الموثقة
-Comment[nl_NL]=Blader door de softwarecollectie van AnduinOS en installeer onze geverifieerde applicaties
-Comment[sv_SE]=Bläddra i AnduinOS programvarusamling och installera våra verifierade applikationer
-Comment[pl_PL]=Przeglądaj kolekcję oprogramowania AnduinOS i instaluj nasze zweryfikowane aplikacje
-Comment[tr_TR]=AnduinOS'un yazılım koleksiyonunu göz atın ve doğrulanmış uygulamalarımızı yükleyin
-Comment[ro_RO]=Explorează colecția de programe a lui AnduinOS și instalează aplicațiile noastre verificate
+Comment=Browse BRGV-OS's software collection and install our verified applications
+Comment[zh_CN]=浏览 BRGV-OS 的软件商店并安装我们验证过的应用
+Comment[zh_TW]=瀏覽 BRGV-OS 的軟體商店並安裝我們驗證過的應用
+Comment[zh_HK]=瀏覽 BRGV-OS 的軟體商店並安裝我們驗證過的應用
+Comment[ja_JP]=BRGV-OS のソフトウェアコレクションを閲覧し、検証済みのアプリケーションをインストールします
+Comment[ko_KR]=BRGV-OS의 소프트웨어 컬렉션을 탐색하고 검증된 애플리케이션을 설치합니다
+Comment[vi_VN]=Duyệt bộ sưu tập phần mềm của BRGV-OS và cài đặt các ứng dụng đã được xác minh của chúng tôi
+Comment[th_TH]=เรียกดูคอลเลกชันซอฟต์แวร์ของ BRGV-OS และติดตั้งแอปพลิเคชันที่ได้รับการตรวจสอบของเรา
+Comment[de_DE]=Durchsuchen Sie die Softwarekollektion von BRGV-OS und installieren Sie unsere verifizierten Anwendungen
+Comment[fr_FR]=Parcourez la collection de logiciels d'BRGV-OS et installez nos applications vérifiées
+Comment[es_ES]=Explore la colección de software de BRGV-OS e instale nuestras aplicaciones verificadas
+Comment[ru_RU]=Просматривайте коллекцию программного обеспечения BRGV-OS и устанавливайте наши проверенные приложения
+Comment[it_IT]=Esplora la collezione di software di BRGV-OS e installa le nostre applicazioni verificate
+Comment[pt_PT]=Explore a coleção de software da BRGV-OS e instale nossos aplicativos verificados
+Comment[pt_BR]=Explore a coleção de software da BRGV-OS e instale nossos aplicativos verificados
+Comment[ar_SA]=تصفح مجموعة البرامج الخاصة بـ BRGV-OS وقم بتثبيت تطبيقاتنا الموثقة
+Comment[nl_NL]=Blader door de softwarecollectie van BRGV-OS en installeer onze geverifieerde applicaties
+Comment[sv_SE]=Bläddra i BRGV-OS programvarusamling och installera våra verifierade applikationer
+Comment[pl_PL]=Przeglądaj kolekcję oprogramowania BRGV-OS i instaluj nasze zweryfikowane aplikacje
+Comment[tr_TR]=BRGV-OS'un yazılım koleksiyonunu göz atın ve doğrulanmış uygulamalarımızı yükleyin
+Comment[ro_RO]=Explorează colecția de programe a lui BRGV-OS și instalează aplicațiile noastre verificate
 Categories=System;
-Exec=xdg-open https://docs.anduinos.com/Applications/Introduction.html
+Exec=
 Terminal=false
 Type=Application
 Icon=system-software-install
