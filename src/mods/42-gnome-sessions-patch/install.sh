d69dafe9d31337eb7612909a7be9a5e1d86afb59
Florin Tanasa <florin.tanasa@gmail.com>
2025-06-30 10:38:04 +0300

    Renames Ubuntu references to BRGV-OS
    Updates session and desktop entry files to reflect the OS branding.
    
    Replaces all occurrences of "Ubuntu" with "BRGV-OS" in relevant configuration files
    located in /usr/share/gnome-session/sessions, /usr/share/xsessions, and
    /usr/share/wayland-sessions. This ensures consistency and proper branding across
    the system.

--- src/mods/42-gnome-sessions-patch/install.sh
+++ src/mods/42-gnome-sessions-patch/install.sh
@@ -3,25 +3,25 @@ set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
 
 print_ok "Setting up /usr/share/gnome-sessions/sessions..."
-sed -i 's/Ubuntu/AnduinOS/g' /usr/share/gnome-session/sessions/ubuntu.session
+sed -i 's/Ubuntu/BRGV-OS/g' /usr/share/gnome-session/sessions/ubuntu.session
 judge "Set up /usr/share/gnome-sessions/sessions"
 
 print_ok "Setting up /usr/share/xsessions..."
 rm /usr/share/xsessions/gnome* || true
 
-mv /usr/share/xsessions/ubuntu.desktop /usr/share/xsessions/anduinos.desktop
-mv /usr/share/xsessions/ubuntu-xorg.desktop /usr/share/xsessions/anduinos-xorg.desktop
-sed -i 's/Name=Ubuntu/Name=AnduinOS/g' /usr/share/xsessions/anduinos.desktop
-sed -i 's/Name=Ubuntu/Name=AnduinOS/g' /usr/share/xsessions/anduinos-xorg.desktop
+mv /usr/share/xsessions/ubuntu.desktop /usr/share/xsessions/brgvos.desktop
+mv /usr/share/xsessions/ubuntu-xorg.desktop /usr/share/xsessions/brgvos-xorg.desktop
+sed -i 's/Name=Ubuntu/Name=BRGV-OS/g' /usr/share/xsessions/brgvos.desktop
+sed -i 's/Name=Ubuntu/Name=BRGV-OS/g' /usr/share/xsessions/brgvos-xorg.desktop
 judge "Set up /usr/share/xsessions"
 
 print_ok "Setting up /usr/share/wayland-sessions..."
 rm /usr/share/wayland-sessions/gnome* || true
 
-mv /usr/share/wayland-sessions/ubuntu.desktop /usr/share/wayland-sessions/anduinos.desktop
-mv /usr/share/wayland-sessions/ubuntu-wayland.desktop /usr/share/wayland-sessions/anduinos-wayland.desktop
-sed -i 's/Name=Ubuntu/Name=AnduinOS/g' /usr/share/wayland-sessions/anduinos.desktop
-sed -i 's/Name=Ubuntu/Name=AnduinOS/g' /usr/share/wayland-sessions/anduinos-wayland.desktop
+mv /usr/share/wayland-sessions/ubuntu.desktop /usr/share/wayland-sessions/brgvos.desktop
+mv /usr/share/wayland-sessions/ubuntu-wayland.desktop /usr/share/wayland-sessions/brgvos-wayland.desktop
+sed -i 's/Name=Ubuntu/Name=BRGV-OS/g' /usr/share/wayland-sessions/brgvos.desktop
+sed -i 's/Name=Ubuntu/Name=BRGV-OS/g' /usr/share/wayland-sessions/brgvos-wayland.desktop
 judge "Set up /usr/share/wayland-sessions"
 
 print_ok "Setting up apparmor to allow user namespaces..."
