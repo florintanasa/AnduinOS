5ea4c82a51a4c030212810ae442d279574f0c731
Florin Tanasa <florin.tanasa@gmail.com>
2025-06-30 10:38:23 +0300

    Configures default templates for new users
    Ensures new users have a set of default templates
    available in their home directory.
    
    Creates language-specific templates if the locale is
    Romanian.

--- src/mods/39-templates-mod/install.sh
+++ src/mods/39-templates-mod/install.sh
@@ -6,16 +6,30 @@ print_ok "Declare Templates dir name as variable for specific languages..."
 case $LANG_MODE in
     "ro_RO")
         export TEMPLATE_DIR="Șabloane"
+        print_ok "Configuring templates..."
+        mkdir -p /etc/skel/$TEMPLATE_DIR
+        touch /etc/skel/$TEMPLATE_DIR/Text.txt
+        touch /etc/skel/$TEMPLATE_DIR/Markdown.md
+cat << 'EOF' > /etc/skel/$TEMPLATE_DIR/Markdown.md
+# Titlu
+
+- [ ] De realizat 1
+- [ ] De realizat 2
+- [ ] De realizat 3
+
+## Subtitlu
+
+1. Lista 1
+2. Lista 2
+3. Lista 3
+EOF
         ;;
     *)
         export TEMPLATE_DIR="Templates"
-        ;;
-esac
-
-print_ok "Configuring templates..."
-mkdir -p /etc/skel/$TEMPLATE_DIR
-touch /etc/skel/$TEMPLATE_DIR/Text.txt
-touch /etc/skel/$TEMPLATE_DIR/Markdown.md
+        print_ok "Configuring templates..."
+        mkdir -p /etc/skel/$TEMPLATE_DIR
+        touch /etc/skel/$TEMPLATE_DIR/Text.txt
+        touch /etc/skel/$TEMPLATE_DIR/Markdown.md
 cat << 'EOF' > /etc/skel/$TEMPLATE_DIR/Markdown.md
 # Title
 
@@ -29,4 +43,6 @@ cat << 'EOF' > /etc/skel/$TEMPLATE_DIR/Markdown.md
 2. Numbered 2
 3. Numbered 3
 EOF
+        ;;
+esac
 judge "Configure templates"
